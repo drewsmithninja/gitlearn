5e89f50089d6e8a5cacee01cf98aea7e24679ca4
drewsmithninja <drew@ninjatechnolabs.com>
2022-06-08 11:05:37 +0530

    zoom image with class component
    Delete logo image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+class App extends Component{
+constructor(props){
+	super(props)
+	
+	// Initializing states
+	this.state = {height:null, width:null}
+	
+	// Bind context of 'this'
+	this.handleZoomIn = this.handleZoomIn.bind(this)
+	this.handleZoomOut = this.handleZoomOut.bind(this)
+	
+	// Create reference of DOM object
+	this.imgRef = React.createRef()
 }
 
-export default App;
+componentDidMount(){
+	// Saving initial dimension of image as class properties
+	this.initialHeight = this.imgRef.current.clientHeight
+	this.initialWidth = this.imgRef.current.clientWidth
+}
+
+// Event handler callback for zoom in
+handleZoomIn(){
+
+	// Fetching current height and width
+	const height = this.imgRef.current.clientHeight
+	const width = this.imgRef.current.clientWidth
+	
+	// Increase dimension(Zooming)
+	this.setState({
+	height : height + 10,
+	width : width + 10,
+	})
+}
+
+// Event handler callback zoom out
+handleZoomOut(){
+
+	// Assigning original height and width
+	this.setState({
+	height : this.initialHeight,
+	width : this.initialWidth,
+	})
+}
+
+render(){
+	// Assign current height and width to the image
+	const imgStyle = { height : this.state.height, width: this.state.width}
+	return(
+	<div>
+		<h2>Zoom Image</h2>
+		{/* Assign reference to DOM element	 */}
+		<img style={imgStyle} ref={this.imgRef} src=
+'https://media.geeksforgeeks.org/wp-content/uploads/20200923125643/download.png' alt='gfg' />
+		<div>
+		<button onClick={this.handleZoomIn}>Zoom In</button>
+		<button onClick={this.handleZoomOut}>Zoom Out</button>
+		</div>
+	</div>
+	)
+}
+}
+export default App
